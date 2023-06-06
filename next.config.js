86120342d76f0082d1dcc3a95c010b4b5e98df90
Drish-xD <drish.xd@gmail.com>
2023-06-06 20:14:38 +0530

    added output folder

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 
 const path = require('path');
 
 const nextConfig = {
+  trailingSlash: true,
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
