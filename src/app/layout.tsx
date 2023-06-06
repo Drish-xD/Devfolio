27301561488416a3b8a3f68def9513e7e9a65b78
Drish-xD <drish.xd@gmail.com>
2023-06-06 19:46:52 +0530

    fix pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import '@/styles/global.scss';
+import { ReactNode } from 'react';
 
 export const metadata = {
   title: 'Drish | Portfolio',
   description: 'Drish'
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
