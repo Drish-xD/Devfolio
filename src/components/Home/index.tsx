014688f0612be1bc1ae94c9b8be0c0f832957d8a
Drish-xD <drish.xd@gmail.com>
2023-07-04 23:01:17 +0530

    responsive home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,24 +2,18 @@ export default function Home() {
   return (
     <section id="home">
       <div className="home_container">
-        <div className="line-wrap one">
-          <h1>I Create</h1>
-        </div>
-        <div className="line-wrap two">
-          <h1>Experiences</h1>
-        </div>
-        <div className="line-wrap mobile">
-          <span>that</span>
-          <h1>Captivate</h1>
-        </div>
-        <div className="line-wrap three">
-          <span>that</span>
-          <h1>Captivate</h1>
-          <p className="intro-line">
-            Hi, I&rsquo;m a front-end developer, blending artistry & technology to deliver stunning
-            online journeys.
-          </p>
-        </div>
+        <h1>
+          <span>I Create</span>
+          <span>Experiences</span>
+          <span>
+            <span>that</span>
+            Captivate
+          </span>
+        </h1>
+        <p className="intro-line">
+          Hi, I&rsquo;m a front-end developer, blending artistry & technology to deliver stunning
+          online journeys.
+        </p>
       </div>
     </section>
   );
