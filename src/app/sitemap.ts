f0cc1828304bf1fb888ba910430a4a2efffc06de
Drish-xD <drish.xd@gmail.com>
2023-07-20 13:28:13 +0530

    generated dynamic sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { getAllProjects } from '@utils/notion';
+
+const URL = 'https://drishxd.dev';
+
+export default async function sitemap() {
+  // static routes
+  const routes = [''].map((route) => ({
+    url: `${URL}${route}`,
+    lastModified: new Date()
+  }));
+
+  // dynamic routes
+  const projects = (await getAllProjects()).map(({ slug }) => ({
+    url: `${URL}/project/${slug}`,
+    lastModified: new Date()
+  }));
+  return [...routes, ...projects];
+}
