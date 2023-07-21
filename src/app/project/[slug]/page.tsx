43d4b58787242261d86f2ce06e937d7f3594bc1c
Drish-xD <drish.xd@gmail.com>
2023-07-21 18:11:59 +0530

    test

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,32 +1,12 @@
 import styles from '@styles/modules/project_page.module.scss';
-import { ProjectProps } from '@types';
-import { nextImport } from '@utils/nextImport';
-import { getSingleProject } from '@utils/notion';
-import { Metadata } from 'next';
-
-const Markdown = nextImport('Markdown');
 
 export default function Project({ params: { slug } }: { params: { slug: string } }) {
+  console.log(slug);
+
   return (
     <main className={`page ${styles.project}`}>
-      <Markdown slug={slug} />
+      <h1>hello world</h1>
+      {/* <Markdown slug={slug} /> */}
     </main>
   );
 }
-
-export const revalidate = 1800;
-
-export const generateMetadata = async ({
-  params: { slug }
-}: {
-  params: { slug: string };
-}): Promise<Metadata> => {
-  const project: ProjectProps = await getSingleProject(slug);
-
-  return {
-    title: project.name,
-    keywords: project.tags,
-    openGraph: { title: project.name, images: project.img },
-    twitter: { title: project.name, images: project.img }
-  };
-};
