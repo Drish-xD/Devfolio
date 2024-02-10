52bd7d36d08d422d1e95007f6abc90c9f0edc340
Drish-xD <drish.xd@gmail.com>
2024-02-11 01:18:45 +0530

    refactor: project card animation and 404

--- src/app/(home)/components/Projects/Card.tsx
+++ src/app/(home)/components/Projects/Card.tsx
@@ -13,33 +13,49 @@ const Card = ({ ...project }: ProjectCardProps) => {
   const { name, tags, image, id, slug } = project;
 
   return (
-    <article ref={(projectRef: HTMLDivElement) => (ref.current[id] = projectRef)}>
+    <article ref={ref}>
       <Image
         src={image}
         alt={`${name} Thumbnail`}
         sizes="(max-width: 425px) 100vw, (max-width: 768px) 70vw, 50vw"
         fill
       />
-      <hgroup>
-        <h3>
-          {Array.from(name).map((letter, i) => (
-            <span key={i}>{letter}</span>
-          ))}
-        </h3>
 
-        <div>
-          {tags.map((tag, i) => (
-            <span key={i}>{tag}</span>
-          ))}
-        </div>
-      </hgroup>
+      <CardInfo name={name} tags={tags} />
 
       <Link href={`/projects/${slug}`} animate={false}>
-        ↗
+        <span>↗</span>
       </Link>
-      <Overlay count={3} name="project" dir="row" />
+
+      <Overlay
+        count={3}
+        name="project"
+        dir="row"
+        bg={{
+          blur: 99,
+          color: '20, 20, 20'
+        }}
+      />
     </article>
   );
 };
 
 export default Card;
+
+const CardInfo = ({ name, tags }: Pick<ProjectCardProps, 'name' | 'tags'>) => {
+  return (
+    <hgroup>
+      <h3>
+        {Array.from(name!).map((letter, i) => (
+          <span key={i}>{letter}</span>
+        ))}
+      </h3>
+
+      <ul>
+        {tags!.map((tag, i) => (
+          <li key={i}>{tag}</li>
+        ))}
+      </ul>
+    </hgroup>
+  );
+};
--- src/components/Overlay/index.tsx
+++ src/components/Overlay/index.tsx
@@ -6,13 +6,23 @@ interface OverlayProps {
   name: string;
   count: number;
   dir: 'row' | 'col';
+  bg?: {
+    color?: string;
+    blur?: number;
+  };
 }
 
-const Overlay: FC<OverlayProps> = ({ name, count, dir }) => {
+const Overlay: FC<OverlayProps> = ({ name, count, dir, bg }) => {
   return (
     <div
       className={`${styles.overlay} ${styles[dir]} overlay_${name}`}
-      style={{ '--count': count } as CSSProperties}
+      style={
+        {
+          '--count': count,
+          '--bgBlur': `${bg?.blur || 25}%`,
+          '--bgColor': bg?.color
+        } as CSSProperties
+      }
     >
       {Array.from({ length: count }, (_, i) => (
         <span key={i} />
