434137ea745ba14beb795f3aaad6516581a48d7c
Drish-xD <drish.xd@gmail.com>
2024-02-15 22:12:14 +0530

    seo: robots and sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,40 @@
+import { MetadataRoute } from 'next';
+
 import { getAllProjects } from '@/utils/contentful';
 
 const URL = 'https://drishxd.dev';
+const STATIC_ROUTES = [''];
 
-export default async function sitemap() {
+/**
+ * Generate sitemap for static and dynamic routes.
+ */
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   // static routes
-  const routes = [''].map((route) => ({
+  const routes: MetadataRoute.Sitemap = STATIC_ROUTES.map((route) => ({
     url: `${URL}${route}`,
-    lastModified: new Date()
+    lastModified: new Date(),
+    changeFrequency: 'never',
+    priority: 1.0
   }));
 
   // dynamic routes
-  const projects = (await getAllProjects()).map(({ slug }) => ({
-    url: `${URL}/project/${slug}`,
-    lastModified: new Date()
-  }));
+  const projects = await projectsSitemap();
+
   return [...routes, ...projects];
 }
+
+/**
+ * Retrieves the sitemap for all projects.
+ */
+const projectsSitemap = async () => {
+  const projects = (await getAllProjects()) || [];
+
+  const projectsSitemap: MetadataRoute.Sitemap = projects.map(({ slug }) => ({
+    url: `${URL}/project/${slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'yearly',
+    priority: 1.0
+  }));
+
+  return projectsSitemap;
+};
