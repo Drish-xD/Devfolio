196c13cfa5297083a7d330c98a8f2f668649557f
Drish-xD <drish.xd@gmail.com>
2024-04-19 23:54:51 +0530

    add: GTM Tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import dynamic from 'next/dynamic';
 import { ReactNode } from 'react';
 
+import { GoogleTagManager } from '@next/third-parties/google';
+
 import { METADATA } from '@/constants';
 import { SmoothScroll, Transition } from '@/providers';
 import '@/styles/global.scss';
-import { fontMain, fontSec } from '@/utils/fonts';
+import { fontMain } from '@/utils/fonts';
 
 const Header = dynamic(() => import('@/components/layout/Header'));
 const Footer = dynamic(() => import('@/components/layout/Footer'));
@@ -13,7 +15,7 @@ const Noise = dynamic(() => import('@/components/layout/Blob/Noise'));
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={`${fontMain.variable} ${fontSec.variable}`}>
+    <html lang="en" className={`${fontMain.variable}`}>
       <body suppressHydrationWarning>
         <SmoothScroll>
           <Transition>
@@ -25,6 +27,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           </Transition>
         </SmoothScroll>
       </body>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID!} />
     </html>
   );
 }
--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,16 +1,8 @@
 import { JetBrains_Mono } from 'next/font/google';
-import localFont from 'next/font/local';
 
 export const fontMain = JetBrains_Mono({
   subsets: ['latin'],
   variable: '--font-main',
-  fallback: ['system-ui', 'monospace'],
-  display: 'swap'
-});
-
-export const fontSec = localFont({
-  src: './fonts/OffBit.woff2',
-  variable: '--font-sec',
   fallback: ['system-ui', 'monospace'],
   display: 'swap'
 });
