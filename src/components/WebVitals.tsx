11b6469ba1ec99bbc7b9317c3efc45526c4e4068
Drish-xD <drish.xd@gmail.com>
2024-04-21 18:12:35 +0530

    fix: web vitals

--- src/components/WebVitals.tsx
+++ src/components/WebVitals.tsx
@@ -6,15 +6,12 @@ import { sendGTMEvent } from '@next/third-parties/google';
 
 export function WebVitals() {
   useReportWebVitals((metric) => {
-    sendGTMEvent({
-      event: 'web_vitals',
-      metric_id: metric.id,
-      metric_name: metric.name,
-      metric_value: Math.round(metric.name === 'CLS' ? metric.value * 1000 : metric.value),
-      metric_rating: metric.rating,
-      navigation_type: metric.navigationType
-    });
-    console.info('WebVitals : ', metric);
+    const webVitals = {
+      ...metric,
+      value: Math.round(metric.name === 'CLS' ? metric.value * 1000 : metric.value)
+    };
+    sendGTMEvent({ event: 'web_vitals', web_vitals: webVitals });
+    console.info('WebVitals : ', webVitals);
   });
 
   return <></>;
